6eb02c36c7c1acc273a543a635a6a68f00efa522
Thomas T <jjtaylor1925@gmail.com>
2022-07-11 01:30:19 -0400

    add `<Leader>bs` mapping

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,2 +1,3 @@
 vim.keymap.set('n', '<Leader>fs', '<cmd>:Telescope find_files<CR>')
 vim.keymap.set('n', '<Leader>fg', '<cmd>:Telescope live_grep<CR>')
+vim.keymap.set('n', '<Leader>bs', '<cmd>:Telescope buffers<CR>')
