69202da7568740dce615d1900c85399809f9ea79
Thomas Taylor <jjtaylor1925@gmail.com>
2022-07-12 01:26:35 -0400

    reorder `vim.opt`

--- .config/nvim/lua/core/options.lua
+++ .config/nvim/lua/core/options.lua
@@ -1,14 +1,17 @@
-vim.opt.number = true
-vim.opt.hidden = true
-vim.opt.wrap = false
-vim.opt.cursorline = true
-vim.opt.clipboard:prepend {"unnamedplus"}
-vim.opt.tabstop = 2
-vim.opt.shiftwidth = 4
-vim.opt.softtabstop = 2
-vim.opt.expandtab = true
-vim.opt.smarttab = true
-vim.opt.showtabline = 2
 vim.opt.background = 'dark'
-vim.opt.termguicolors = true
+vim.opt.clipboard:prepend {"unnamedplus"}
+vim.opt.cursorline = true
 vim.opt.completeopt = {'menu', 'menuone', 'noselect'}
+vim.opt.expandtab = true
+vim.opt.hidden = true
+vim.opt.number = true
+vim.opt.shiftwidth = 4
+vim.opt.showtabline = 2
+vim.opt.smarttab = true
+vim.opt.softtabstop = 2
+vim.opt.splitbelow = true
+vim.opt.splitright = true
+vim.opt.tabstop = 2
+vim.opt.termguicolors = true
+vim.opt.wrap = false
+
