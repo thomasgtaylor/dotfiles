c433ee572b1dbe4066cc6f66e814fb15bc37d8a6
Thomas T <jjtaylor1925@gmail.com>
2022-08-28 00:44:29 -0400

    update `telescope.lua`

--- .config/nvim/lua/config/telescope.lua
+++ .config/nvim/lua/config/telescope.lua
@@ -1,4 +1,13 @@
+local telescope = require('telescope')
+
+telescope.setup({
+  defaults = {
+    file_ignore_patterns = {
+      "node_modules"
+    }
+  }
+})
+
 vim.keymap.set('n', '<Leader>fs', '<cmd>:Telescope find_files<CR>')
 vim.keymap.set('n', '<Leader>fg', '<cmd>:Telescope live_grep<CR>')
 vim.keymap.set('n', '<Leader>fb', '<cmd>:Telescope buffers<CR>')
-
