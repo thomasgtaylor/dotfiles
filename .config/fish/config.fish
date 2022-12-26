b40ec49debd0af6600ddbb91f0a07a93950737fa
Thomas T <jjtaylor1925@gmail.com>
2022-12-26 01:50:55 -0500

    add `fish` configuration

---
+++ .config/fish/config.fish
@@ -0,0 +1,14 @@
+set -g hydro_symbol_prompt "➜"
+set -g fish_prompt_pwd_dir_length 0
+set -g hydro_color_prompt "E5C890"
+set -g hydro_color_pwd "A6D189"
+set -g hydro_color_git "F4B8E4"
+
+set -gx EDITOR nvim
+set -gx VISUAL nvim
+
+alias vi="nvim"
+alias vim="nvim"
+alias cat="bat"
+
+fish_add_path $HOME/bin
